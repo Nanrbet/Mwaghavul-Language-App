42db8af1acf380b20354aac07b50d6320a69b153
Nanribet Fwangje <fwangje@ualberta.ca>
2024-07-22 22:57:26 -0600

    Updated Bookmark first populated ArrayList and deletion

--- app/src/main/java/com/example/mwaghavullexicon/MainActivity.kt
+++ app/src/main/java/com/example/mwaghavullexicon/MainActivity.kt
@@ -1,22 +1,25 @@
 package com.example.mwaghavullexicon
 
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.mwaghavullexicon.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var fragmentManager: FragmentManager
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,18 +40,41 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 binding.buttomNavigation.menu.findItem(R.id.nav_bookmark).setIcon(R.drawable.round_star_outline_24)
             }
             when(item.itemId){
-                R.id.nav_home -> openFragment(HomeFragment())
-                R.id.nav_search -> openFragment(SearchFragment())
-                R.id.nav_bookmark -> {openFragment(BookmarkFragment())
+                R.id.nav_home -> openFragment(HomeFragment(), true)
+                R.id.nav_search -> openFragment(SearchFragment(), true)
+                R.id.nav_bookmark -> {openFragment(BookmarkFragment(), true)
                         // Change the icon to filled star
                         item.setIcon(R.drawable.filled_star_24)}
-                R.id.nav_history -> openFragment(HistoryFragment())
+                R.id.nav_history -> openFragment(HistoryFragment(), true)
             }
             true
         }
         fragmentManager = supportFragmentManager
-        openFragment(HomeFragment())
-
+        openFragment(HomeFragment(), true)
+        bottomNavigationView = findViewById(R.id.buttom_navigation)
+        // Load the default fragment
+        if (savedInstanceState == null) {
+            bottomNavigationView.selectedItemId = R.id.nav_home
+        }
+        // Register the custom OnBackPressedCallback
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    binding.drawerLayout.closeDrawer(GravityCompat.START)
+                } else {
+                    val fragmentManager = supportFragmentManager
+                    if (fragmentManager.backStackEntryCount > 1) {
+                        fragmentManager.popBackStack()
+                        val currentFragment = fragmentManager.fragments.last()
+                        updateBottomNavigation(currentFragment)
+                    }else if (fragmentManager.backStackEntryCount == 1) {
+                        fragmentManager.popBackStack()
+                    }else {
+                        finish()
+                    }
+                }
+            }
+        })
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -65,25 +91,35 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 // Handle help action
                 //TODO
             }
-            R.id.nav_about -> {openFragment(AboutFragment())}
+            R.id.nav_about -> {openFragment(AboutFragment(), false)}
         }
         binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
 
     // To close drawer if back button is pressed
-    @Deprecated("This method has been deprecated in favor of using the\n      {@link OnBackPressedDispatcher} via {@link #getOnBackPressedDispatcher()}.\n      The OnBackPressedDispatcher controls how back button events are dispatched\n      to one or more {@link OnBackPressedCallback} objects.")
-    override fun onBackPressed() {
-        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
-            binding.drawerLayout.closeDrawer(GravityCompat.START)
-        }else {
-            super.onBackPressedDispatcher.onBackPressed()
+//    @Deprecated("This method has been deprecated in favor of using the\n      {@link OnBackPressedDispatcher} via {@link #getOnBackPressedDispatcher()}.\n      The OnBackPressedDispatcher controls how back button events are dispatched\n      to one or more {@link OnBackPressedCallback} objects.")
+//    override fun onBackPressed() {
+//        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+//            binding.drawerLayout.closeDrawer(GravityCompat.START)
+//        }else {
+//            super.onBackPressedDispatcher.onBackPressed()
+//        }
+//    }
+    private fun updateBottomNavigation(fragment: Fragment) {
+        when (fragment) {
+            is HomeFragment -> bottomNavigationView.selectedItemId = R.id.nav_home
+            is SearchFragment -> bottomNavigationView.selectedItemId = R.id.nav_search
+            is BookmarkFragment -> bottomNavigationView.selectedItemId = R.id.nav_bookmark
+            is HistoryFragment -> bottomNavigationView.selectedItemId = R.id.nav_history
         }
     }
     // To not keep repeating the code to change the fragments on the frame layout of the main activity
-    private fun openFragment(fragment: Fragment){
+    private fun openFragment(fragment: Fragment, isTop: Boolean){
         val fragmentTransaction: FragmentTransaction= fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.main_fragment_container,  fragment)
+        fragmentTransaction.replace(R.id.main_fragment_container, fragment)
+            if (!isTop)
+             fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
     }
 
--- app/src/main/java/com/example/mwaghavullexicon/SearchFragment.kt
+++ app/src/main/java/com/example/mwaghavullexicon/SearchFragment.kt
@@ -1,6 +1,8 @@
 package com.example.mwaghavullexicon
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
@@ -8,13 +10,16 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.core.view.MenuProvider
 
 
 class SearchFragment : Fragment() {
+
+    private lateinit var listView: ListView
+    private lateinit var adapter: ArrayAdapter<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,6 +38,18 @@ class SearchFragment : Fragment() {
     // Inflate the menu
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.main_options, menu)
+        //tODO: make menuSetting =menu.findItem(R.id.action_setting)
+        // so that translation can change icon when selected
+        val id = Global.getState(requireContext() as MainActivity, "dict_type")
+        if (id != null) {
+            val menuItem = menu.findItem(resources.getIdentifier(id, "id", requireContext().packageName))
+            if (menuItem != null) {
+                onOptionsItemSelected(menuItem)
+            } else {
+                Log.e("SearchFragment", "Menu item not found: $id")
+                // Handle the error appropriately, e.g., show a message to the user
+            }
+        }
         super.onCreateOptionsMenu(menu, inflater)
     }
 
@@ -40,17 +57,59 @@ class SearchFragment : Fragment() {
         return when (item.itemId) {
             R.id.mwaghavul_english -> {
                 Toast.makeText(requireContext(), "Mwaghavul - English clicked", Toast.LENGTH_LONG).show()
+                Global.saveState(requireActivity() as MainActivity, "dict_type", "mwaghavul_english")
                 true
             }
             R.id.english_mwaghavul -> {
                 Toast.makeText(requireContext(), "English - Mwaghavul clicked", Toast.LENGTH_LONG).show()
+                Global.saveState(requireActivity() as MainActivity, "dict_type", "english_mwaghavul")
                 true
             }
             R.id.english_english -> {
                 Toast.makeText(requireContext(), "English - English clicked", Toast.LENGTH_LONG).show()
+                Global.saveState(requireActivity() as MainActivity, "dict_type", "english_english")
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val listView = view.findViewById<ListView>(R.id.dictionary_search_list)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, getListOfWords())
+        listView.adapter = adapter
+
+        // Set the item click listener
+        listView.setOnItemClickListener { parent, view, position, id ->
+            // Create an instance of DetailFragment
+            val fragment = DetailFragment()
+
+            // Optionally, pass data to the DetailFragment using arguments
+            val bundle = Bundle()
+            bundle.putString("selected_word", adapter.getItem(position))
+            fragment.arguments = bundle
+
+            // Perform the fragment transaction
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.main_fragment_container, fragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+    }
+    fun getListOfWords() : Array<String>{
+        val source = arrayOf(
+            "a","apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew",
+            "kiwi", "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry",
+            "strawberry", "tangerine", "ugli", "vanilla", "watermelon", "xigua", "yam", "zucchini",
+            "apricot", "blueberry", "cantaloupe", "dragonfruit", "eggplant"
+        )
+        return source
     }
 }
